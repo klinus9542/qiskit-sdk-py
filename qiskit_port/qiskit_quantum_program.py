a80a962a97461acf92d88005334a5d657dc9ae70
wildfire <wildfire810@gmail.com>
2017-11-03 22:08:46 +1100

    增加了更长的超时时间参数

--- qiskit_port/qiskit_quantum_program.py
+++ qiskit_port/qiskit_quantum_program.py
@@ -8,6 +8,9 @@ qp.set_api(
 backend = sys.argv[2]
 coupling_map = eval(sys.argv[3])
 qp.load_qasm_file(sys.argv[4], 'qiskit')
-result = qp.execute(["qiskit"], backend=backend, coupling_map=coupling_map, shots=int(sys.argv[5]))
-if result.get_status() == 'COMPLETED':
-     print(result.get_counts("qiskit"))
+result = qp.execute(["qiskit"], backend=backend, coupling_map=coupling_map, shots=int(sys.argv[5]), wait=50, timeout=600)
+if str(result) == 'COMPLETED':
+    print(result.get_counts("qiskit"))
+    print(result.get_ran_qasm("qiskit"))
+else:
+    print(result)
